291bddb52b18c799a0b80468f7a67b57c6a7f577
Hornwitser <github@hornwitser.no>
2020-07-22 12:08:38 +0200

    Rework main.js
    Move the requires to the top of the file and pull the the creation of
    the file listeners into to the start function.

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 "use strict";
 require('dotenv').config();
-const file_listener = require("./file_listener.js");
-const servers = JSON.parse((process.env.Servers));
-
-const file_events = file_listener.watch_files(servers);
-
 const Rcon = require("rcon-client").Rcon;
+
+const file_listener = require("./file_listener.js");
+const init = require(process.env.Is_lobby === 'true' ? "./server.js" : "./client.js");
+
+
 async function connect_rcon(port, pw) {
     const rcon = new Rcon({ host: "localhost", port: port, password: pw });
     await rcon.connect();
@@ -13,23 +13,24 @@ async function connect_rcon(port, pw) {
 }
 
 async function start() {
+    const servers = JSON.parse((process.env.Servers));
+    const file_events = file_listener.watch_files(servers);
+
     const locals_rcons = {};
     for (let [ip, server] of Object.entries(servers["local_servers"])) {
         locals_rcons[ip] = await connect_rcon(server.Rcon_port, server.Rcon_pass);
     }
+
     if (process.env.Is_lobby === 'true') {
-        //eslint-disable-next-line global-require
-        const { init } = require('./server.js');
         const lobby = servers["lobby"];
         const lobby_rcon = await connect_rcon(lobby.Rcon_port, lobby.Rcon_pass);
         await init(lobby_rcon, locals_rcons, file_events);
+
     } else {
-        //eslint-disable-next-line global-require
-        const { init } = require('./client.js');
         await init(locals_rcons, file_events);
     }
-
 }
+
 if (require.main === module) {
     start().catch(err => {
         console.error(err);
